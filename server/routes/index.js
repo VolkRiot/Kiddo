84846e2cb5db8dfca5e9029f81450444f0851215
martinsflavio <flaviodosreismartins@gmail.com>
2017-06-24 14:04:18 -0700

    server

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express'),
+      router  = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  response.sendFile(path.resolve(__dirname, '../../public/index.html'))
+});
+
+module.exports = router;
