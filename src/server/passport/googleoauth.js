25379c4e6c2f59c6075ee3fb8fa424da47a5455f
sfrunner <sfrunner1188@gmail.com>
2017-06-27 15:45:35 -0700

    Initial Functioning Passport to be able to retrieve oauth token

---
+++ src/server/passport/googleoauth.js
@@ -0,0 +1,35 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+// Configure the Google strategy for use by Passport.js.
+//
+// OAuth 2-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Google API on the user's behalf,
+// along with the user's profile. The function must invoke `cb` with a user
+// object, which will be set at `req.user` in route handlers after
+// authentication.
+module.exports = function(){
+
+passport.use(new GoogleStrategy({
+  clientID: '161301570635-fv4tliniaqa4atpj2ldelicb7qlo7of4.apps.googleusercontent.com',
+  clientSecret: 'aLp3Uh7OOs_E6aw8ulayB5jX',
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+  accessType: 'offline'
+}, (accessToken, refreshToken, profile, cb) => {
+  // Extract the minimal profile information we need from the profile object
+  // provided by Google
+  console.log(accessToken);
+  console.log(refreshToken);
+  console.log(profile.displayName);
+  cb(null, null);
+}));
+// used to serialize the user for the session
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+// used to deserialize the user
+passport.deserializeUser((obj, cb) => {
+  cb(null, obj);
+});
+
+}
