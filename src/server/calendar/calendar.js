c354924ba5221131123c4b666585a6ee5f283eaa
sfrunner <sfrunner1188@gmail.com>
2017-06-28 21:20:36 -0700

    Able to pull calendar Ids of signed in user

--- src/server/calendar/calendar.js
+++ src/server/calendar/calendar.js
@@ -1,18 +1,30 @@
-console.log(process.env);
-
-var listCalendars = function(){
-    var calendar = google.calendar('v3');
-    calendar.calendarList.list({
-        auth:'ya29.Glx3BD5aEkdqNnV5JjTBLArLgnnEGdIzRlyY1U8210Be1OCGqtJyhIvUQpc-Zdgsq-mUESSi6flOOryULswtGnibSXuXAAJJhcVcv2aI_ALsBxhCJKE2XSiFQSpQRA',
-        pageToken: 1,
-        minAccessRole: 'writer'
-    }, function(err,response){
-        if (err){
-            console.log(err);
-        }
-        var list = response.items
-        console.log(list);
-    });
+var google = require('googleapis');
+listEvents();
+function listEvents() {
+  var calendar = google.calendar('v3');
+  calendar.events.list({
+    auth: 'Yw-VD9apjxS7JyJdrytLBAzv68xDvygcgqCK1TvAx5p7StvHvZnYgoXRk__vq_3O23ssOq5vkLwuM5ehBw_CiVVMmQ',
+    calendarId: 'sfrunner1188@gmail.com',
+    timeMin: (new Date()).toISOString(),
+    maxResults: 10,
+    singleEvents: true,
+    orderBy: 'startTime'
+  }, function(err, response) {
+    if (err) {
+      console.log('The API returned an error: ' + err);
+      return;
+    }
+    var events = response.items;
+    if (events.length == 0) {
+      console.log('No upcoming events found.');
+    } else {
+      console.log('Upcoming 10 events:');
+      for (var i = 0; i < events.length; i++) {
+        var event = events[i];
+        var start = event.start.dateTime || event.start.date;
+        console.log('%s - %s', start, event.summary);
+      }
+    }
+  });
 }
 
-module.exports = listCalendars;
