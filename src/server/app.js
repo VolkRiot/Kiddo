953647804d3e29c0320cd2135d3371a2f233e760
sfrunner <sfrunner1188@gmail.com>
2017-06-28 21:27:39 -0700

    Error Handling uncommented

--- src/server/app.js
+++ src/server/app.js
@@ -39,15 +39,15 @@ app.use((req, res, next) => {
 });
 
 // error handler
-//app.use((err, req, res, next) => {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  //res.locals.message = err.message;
-  //res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  //res.status(err.status || 500);
-  //res.sendStatus(500);
-//});
+  res.status(err.status || 500);
+  res.sendStatus(500);
+});
 
 
 
