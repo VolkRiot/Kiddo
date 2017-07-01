f97bf07960a2872825f06ee96484b56f4f12b8cd
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-06-30 18:27:33 -0700

    landing page not done but enough

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -5,8 +5,8 @@ import Home from './components/Home'
 class App extends React.Component {
   render(){
     return (
-        <div>
-          <h1>Hello React!</h1>
+        <div className="main">
+          <h1 className="title">Kiddo</h1>
           <div className="container">
             <Home/>
           </div>
