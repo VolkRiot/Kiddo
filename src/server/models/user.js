465052f7dc8f056bb1b6c423e836e79a6f6f33d6
martinsflavio <flaviodosreismartins@gmail.com>
2017-07-02 03:05:39 -0700

    crating api routes and controllers

---
+++ src/server/models/user.js
@@ -0,0 +1,22 @@
+const mongoose  = require('../db/mongodb');
+
+
+const UserSchema = new mongoose.Schema({
+      email: {
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: true
+      },
+      password: {
+        type: String,
+        required: true
+      },
+      timestamp: {
+        type: Date,
+        default: Date.now
+      }
+    });
+
+
+module.exports = mongoose.model('Users', UserSchema);
---
+++ src/server/routes/api.js
@@ -0,0 +1,177 @@
+const express = require('express'),
+      router  = express.Router();
+
+const controllers = require('../controllers');
+
+
+/* GET */
+/*
+  Example : http://.../api/user?_id=5958... = to id matching doc
+  or      : http://.../api/user             = to all docs
+ */
+router.get('/:collection', (req, res, next) => {
+  let collection   = req.params.collection,
+      controller = controllers[collection],
+      id         = req.query._id;
+
+  if (controller === undefined) {
+    return next({message: 'Invalid Query' , err: `Param : ${collection}`});
+
+  }
+
+
+  if (id !== undefined) {
+    controller.findById( id, (err, results) => {
+      if (err) {
+        return next({message: 'Invalid Query' , err: `Invalid ${collection} ID`});
+
+      } else if (results === null){
+        return res.status(200).json({message: 'Not Found!', body: null});
+
+      } else {
+        return res.status(200).json({message: 'Success', body: results});
+      }
+    });
+
+
+  } else {
+    controller.find({}, (err, results) => {
+      if (err) {
+        return next({message: 'Invalid Query', err: err});
+
+      } else if (results === null){
+        return res.status(200).json({message: 'Not Found!', body: null});
+
+      } else {
+        return res.status(200).json({message: 'Success', body: results});
+      }
+    });
+  }
+
+
+});
+///////////////////////////////////////////////////
+
+/* POST */
+/*
+  Example: http://.../api/user?method=create
+  body : { obj to be saved }
+*/
+router.post('/:collection', (req, res, next) => {
+  let collection = req.params.collection,
+      method     = req.query.method,
+      controller = controllers[collection],
+      body       = req.body;
+
+  if (controller === undefined) {
+    return next({message: 'Invalid Query' , err: `Param : ${collection}`});
+
+  } else if (controller[method] === undefined) {
+    return next({message: 'Invalid Query', err: `Param : ${ method || 'Missing' }`});
+
+  } else if (Object.keys(body).length === 0 && body.constructor === Object){
+    return next({message: 'Invalid Query', err: `Body : ${ 'Missing' }`});
+  }
+
+  if (method === 'create') {
+    controller.create(body, (err, results) => {
+      if (err) {
+        //11000 is MongoDB code for duplicate
+        if (err.code === 11000) {
+          return next('User already Exist\'s! ');
+        } else {
+          return next(err);
+        }
+      } else {
+        return res.status(200).json({message: 'Success', body: results});
+      }
+    });
+  } else {
+    return next({message: 'Invalid Query', err: `Invalid param for POST Request: ${method}`});
+  }
+});
+///////////////////////////////////////////////////
+
+/* PUT */
+/*
+  Example: http://.../api/user?method=update&_id=59589...
+  body : { obj with fields to be updated }
+*/
+router.put('/:collection', (req, res, next) => {
+  let collection = req.params.collection,
+      method     = req.query.method,
+      controller = controllers[collection],
+      id         = req.query._id,
+      body       = req.body;
+
+  if (controller === undefined) {
+    return next({message: 'Invalid Query', err: `Invalid param : ${collection}`});
+
+  } else if (controller[method] === undefined) {
+    return next({message: 'Invalid Query', err: `Invalid param : ${method || 'Missing'}`});
+
+  } else if (Object.keys(body).length === 0 && body.constructor === Object){
+    return next({message: 'Invalid Query', err: `Body : ${ 'Missing' }`});
+  }
+
+
+  if (method === 'update') {
+    if (id !== undefined) {
+      controller.update(id, body, (err, results) => {
+        if (err) {
+          return next({message: `Fail on Update ${collection}`, err: `${collection} not found!`});
+
+        } else {
+          return res.status(200).json({message: 'Success', body: results});
+        }
+      });
+
+    } else {
+      return next({message: 'Invalid Query', err: `Missing ${collection} ID`});
+    }
+
+  } else {
+    return next({message: 'Invalid Query', err: `Invalid Method for PUT Request: ${method}`});
+  }
+});
+///////////////////////////////////////////////////
+
+/* DELETE */
+/*
+ Example: http://.../api/user?method=delete&_id=59589...
+ */
+router.delete('/:collection', (req, res, next) => {
+  let collection = req.params.collection,
+      method     = req.query.method,
+      controller = controllers[collection],
+      id         = req.query._id;
+
+
+  if (controller === undefined) {
+    return next({message: 'Invalid Query', err: `Invalid param : ${collection}`});
+
+  } else if (controller[method] === undefined) {
+    return next({message: 'Invalid Query', err: `Invalid param : ${method || 'Missing'}`});
+
+  }
+
+
+  if (method === 'destroy') {
+    if (id !== undefined) {
+      controller.destroy(id, (err, results) => {
+        if (err) {
+          return next({message: 'Fail on delete', err: `${collection} not found!`});
+        } else {
+          return res.status(200).json({message: (results?'Success':'Not Found'), body: results});
+        }
+      })
+    } else {
+      return res.status(500).json({message: 'fail', err: 'missing id'});
+    }
+  } else {
+    return res.status(500).json({message: 'fail', err: `Invalid Method for DELETE Request: ${method}`});
+  }
+});
+///////////////////////////////////////////////////
+
+module.exports = router;
