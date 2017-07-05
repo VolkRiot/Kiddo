018db0c96e411ee9d12bfc0e5c9dd1024540bfef
Mikhail Metrikin <metrikin@gmail.com>
2017-07-04 17:31:07 -0700

    Enable the JSX Home view button to link to Google Login

--- src/client/app/components/HomeFooter.jsx
+++ src/client/app/components/HomeFooter.jsx
@@ -1,28 +1,22 @@
-import React from 'react'
-
+import React from 'react';
 
 class HomeFooter extends React.Component {
-	render(){
-
-		return(
-			
-     		 <div className="footer navbar-fixed-bottom" id="footer">
-     			<button type="button" className="btn btn-outline-secondary btn-lg" id="signIn">Sign In With Google >></button>
-     			
-     		 </div>
-
-			)
-	}
+  render() {
+    return (
+      <div className="footer navbar-fixed-bottom" id="footer">
+        {/* TODO:(Rima) Workaround for now, maybe want to change the button to an a tag
+          but for sake of styling warpped in a tag for now */}
+        <a href="/auth/google">
+          <button
+            type="button"
+            className="btn btn-outline-secondary btn-lg"
+            id="signIn"
+          >
+            Sign In With Google >>
+          </button>
+        </a>
+      </div>
+    );
+  }
 }
 export default HomeFooter;
-
-
-
-
-
-
-
-
-
-
-
