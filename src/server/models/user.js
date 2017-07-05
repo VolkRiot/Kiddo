5574eeae4af83444b5e00a124d4a99a2a3d8c461
Mikhail Metrikin <metrikin@gmail.com>
2017-07-04 18:42:15 -0700

    Cleaning more issues and .gitignore .vscode

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,22 +1,20 @@
-const mongoose  = require('../db/mongodb');
-
+const mongoose = require('../db/mongodb');
 
 const UserSchema = new mongoose.Schema({
-      email: {
-        type: String,
-        lowercase: true,
-        unique: true,
-        required: true
-      },
-      password: {
-        type: String,
-        required: true
-      },
-      timestamp: {
-        type: Date,
-        default: Date.now
-      }
-    });
-
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now
+  }
+});
 
 module.exports = mongoose.model('Users', UserSchema);
--- src/server/passport/googleoauth.js
+++ src/server/passport/googleoauth.js
@@ -8,26 +8,28 @@ var GoogleStrategy = require('passport-google-oauth20').Strategy;
 // along with the user's profile. The function must invoke `cb` with a user
 // object, which will be set at `req.user` in route handlers after
 // authentication.
-module.exports = function(){
-
-passport.use(new GoogleStrategy({
-  clientID: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: process.env.CALLBACKURL,
-  accessType: 'offline'
-}, (accessToken, refreshToken, profile, cb) => {
-  //Send Access Token and Profile Information to Database
-  console.log('Access Token: ' + accessToken);
-  cb(null, null);
-
-}));
-// used to serialize the user for the session
-passport.serializeUser((user, cb) => {
-  cb(null, user);
-});
-// used to deserialize the user
-passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
-});
-
-}
+module.exports = function() {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: process.env.CALLBACKURL,
+        accessType: 'offline'
+      },
+      (accessToken, refreshToken, profile, cb) => {
+        // Send Access Token and Profile Information to Database
+        console.log('Access Token: ' + accessToken);
+        cb(null, null);
+      }
+    )
+  );
+  // used to serialize the user for the session
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  // used to deserialize the user
+  passport.deserializeUser((obj, cb) => {
+    cb(null, obj);
+  });
+};
