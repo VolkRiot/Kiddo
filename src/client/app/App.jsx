f02a6935e5a643303ef4bfa793c4418fd12464ef
Mikhail Metrikin <VolkRiot@users.noreply.github.com>
2017-07-09 16:50:49 -0700

    Revert "Steven branch"

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -7,7 +7,6 @@ import Home from './components/Home';
 import  NotFound from './components/NotFound';
 import Profile from './components/Profile';
 import Add from './components/Add';
-import Calendar from './components/Calendar';
 
 import '../index.css';
 
