a3b4a8e7a57e2cd474d66b207fccba54f230f30c
Mikhail Metrikin <metrikin@gmail.com>
2017-07-09 17:28:08 -0700

    Basic Routing w/HashRouter

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -1,23 +1,24 @@
 'use strict';
 
-import React from 'react';
-import {HashRouter, Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
-import  NotFound from './components/NotFound';
+import NotFound from './components/NotFound';
 import Profile from './components/Profile';
 import Add from './components/Add';
 
 import '../index.css';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-       <Home />
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route exact path='/profile' component={Profile}/>
+        </Switch>
     );
   }
 }
 
 export default App;
-
-
