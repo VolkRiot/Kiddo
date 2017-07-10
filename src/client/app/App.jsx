f340b4e07974ef9f365231b93373026f0f93b425
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-07-09 17:33:29 -0700

    Kid profile layout MVP done, needs finishing touches

--- src/client/app/App.jsx
+++ src/client/app/App.jsx
@@ -3,17 +3,26 @@
 import React from 'react';
 import {HashRouter, Route, Switch} from 'react-router-dom';
 
-import Home from './components/Home';
+import Home from './components/landing/Home';
 import  NotFound from './components/NotFound';
-import Profile from './components/Profile';
-import Add from './components/Add';
+import Profile from './components/dashboard/Profile';
+import Add from './components/add-child/Add';
+import Kid from './components/kid-view/Kid';
 
 import '../index.css';
 
 class App extends React.Component {
   render() {
     return (
-       <Home />
+       <HashRouter>
+        <Switch>
+          <Route exact path="/" component={ Home }/>
+          <Route path="/profile" component={ Profile }/>
+          <Route path="/add" component={Add }/>
+          <Route path="/kid" component={Kid }/>
+          <Route component={ NotFound } />
+        </Switch>
+      </HashRouter> 
     );
   }
 }
