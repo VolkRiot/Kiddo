c1cbfa0a037f8a3c40d00f7d673e3bb50a4901a3
Mikhail Metrikin <metrikin@gmail.com>
2017-07-16 19:08:46 -0700

    Minor fix for routing strategy for current user

--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -5,11 +5,11 @@ const express   = require('express'),
 
 // (TODO): This sux too should be consolidated with the api model Flavio made but it is an issue.
 router.get('/everything', (req, res) => {
-    User.find({})
+    User.findById(req.user._id)
       .populate('kids')
         .exec((err, response) => {
           if (err) res.status(500).send('Server failed to return user');
-          res.status(200).json(response[0]);
+          res.status(200).json(response);
         });
 });
 
