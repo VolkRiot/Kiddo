f2af7bd8513ae30aac3db08031755d708d5c22b4
Mikhail Metrikin <metrikin@gmail.com>
2017-07-18 16:15:45 -0700

    Navigation issue resolved

--- Kiddo-Mobile/containers/Main.js
+++ Kiddo-Mobile/containers/Main.js
@@ -7,12 +7,14 @@ import {
   ActivityIndicator,
   Dimensions
 } from 'react-native';
-import RegisterApp from '../components/registerApp';
 
 // import Proptypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 // import SocketIOClient from 'socket.io-client';
+
+import RegisterApp from '../components/registerApp';
+import NavigationProvider from './Navigator';
 
 class Main extends Component {
   render() {
