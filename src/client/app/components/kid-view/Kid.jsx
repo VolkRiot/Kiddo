38eb427f7d449518c8952c5713f5b327d776324f
Mikhail Metrikin <metrikin@gmail.com>
2017-07-19 15:37:40 -0700

    Default kiddo

--- src/client/app/components/kid-view/Kid.jsx
+++ src/client/app/components/kid-view/Kid.jsx
@@ -5,16 +5,28 @@ import KidNote from './KidNote';
 import KidShop from './KidShop';
 import KidReminder from './KidReminder';
 
-
 class Kid extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { defaultKid : {
+				photo: './img/girl.png',
+				firstName: ''
+			} };
+	}
 	render(){
 		return (
 			<div className="kid">
 				<div className="row">
 					<div className="col-lg-4">
-						<img src={ this.props.kiddo ? this.props.kiddo.avatar.url : './img/girl.png'} style={{width:'74px', height: '74px', borderRadius:'50%'}} />
+						<img src={ this.props.kiddo ?
+							this.props.kiddo.avatar.url :
+							this.state.defaultKid.photo }
+							style={{width:'74px', height: '74px', borderRadius:'50%'}}
+						/>
 					</div>
-					<div className="col-lg-4" id="kidTitle"> { this.props.kiddo ? this.props.kiddo.firstName : '' }
+					<div className="col-lg-4" id="kidTitle"> { this.props.kiddo ?
+						this.props.kiddo.firstName :
+						this.state.defaultKid.firstName }
 					</div>
 					<div className="col-lg-4"> Dropdown
 					</div>
