68aedc84eaed380cb6fb8667390da94e16f6cd10
Mikhail Metrikin <metrikin@gmail.com>
2017-07-21 16:16:44 -0700

    Revert "Merge pull request #102 from VolkRiot/stevenBranch" Undo this shit This reverts commit 1364673a648871552183cdc2e7ed992176802fd8, reversing changes made to 095a9aa60c5f96906d09e9e027089ca89f425b8f.

--- src/client/app/utils/apiHelper.js
+++ src/client/app/utils/apiHelper.js
@@ -7,8 +7,6 @@ const ApiHelper = () => {
   const addKiddoRoute = '/api/kid?method=create';
   const addEventRoute = '/api/event?method=create';
   const addCalendarRoute = '/kid/addcalendar';
-  const retrieveCalEvents = '/calendar/getevents';
-  const findKidByIdRoute = '/api/kid?_id=';
 
   const putKidRoute = id => `/api/kid?method=update&_id=${id}`;
 
@@ -27,13 +25,7 @@ const ApiHelper = () => {
     },
     addCalendar: kidName => {
       return api.post(addCalendarRoute, kidName);
-    }, 
-    eventsSnapshot: () => {
-      return api.get(retrieveCalEvents);
     },
-    // getKiddo: kiddo_id => {
-    //   return api.get(findKidByIdRoute + kiddo_id);
-    // }
     updateKiddo: kiddo => {
       return api.put(putKidRoute(kiddo._id), kiddo);
     }
