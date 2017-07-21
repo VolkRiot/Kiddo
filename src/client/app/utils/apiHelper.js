41a28a56c605b346169a709b95dc45bf4e1de218
Mikhail Metrikin <metrikin@gmail.com>
2017-07-21 16:14:52 -0700

    Revert "Tiny cleanup" Reverting commit This reverts commit e0d2d20a9f4ba113be7238c79741491dfb842a18.

--- src/client/app/utils/apiHelper.js
+++ src/client/app/utils/apiHelper.js
@@ -8,6 +8,7 @@ const ApiHelper = () => {
   const addEventRoute = '/api/event?method=create';
   const addCalendarRoute = '/kid/addcalendar';
   const retrieveCalEvents = '/calendar/getevents';
+  const findKidByIdRoute = '/api/kid?_id=';
 
   const putKidRoute = id => `/api/kid?method=update&_id=${id}`;
 
@@ -26,10 +27,13 @@ const ApiHelper = () => {
     },
     addCalendar: kidName => {
       return api.post(addCalendarRoute, kidName);
-    },
+    }, 
     eventsSnapshot: () => {
       return api.get(retrieveCalEvents);
     },
+    // getKiddo: kiddo_id => {
+    //   return api.get(findKidByIdRoute + kiddo_id);
+    // }
     updateKiddo: kiddo => {
       return api.put(putKidRoute(kiddo._id), kiddo);
     }
