0137fc6c5d90c7b36b426f416c1c69242117e933
Mikhail Metrikin <metrikin@gmail.com>
2017-07-23 14:17:05 -0700

    Privatize the routes

--- src/server/routes/auth.js
+++ src/server/routes/auth.js
@@ -1,29 +1,35 @@
-const express              = require('express'),
-      router               = express.Router(),
-      passport             = require('passport'),
-      UserController       = require('./../controllers').user,
-    { /*isLoggedIn,*/
-      isUser,
-      isAuthenticated }    = require('./helpers/isLoggedInCheck');
+const express = require('express'),
+  router = express.Router(),
+  passport = require('passport'),
+  UserController = require('./../controllers').user,
+  {
+    /*isLoggedIn,*/
+    isUser,
+    isAuthenticated
+  } = require('./helpers/isLoggedInCheck');
 
-router.get('/google/callback',
+router.get(
+  '/google/callback',
   passport.authenticate('google', {
-    successRedirect : '/#/dashboard',
-    failureRedirect : '/'
-}));
+    successRedirect: '/#/dashboard',
+    failureRedirect: '/'
+  })
+);
 
-router.get('/google', passport.authenticate('google',
-  { scope :
-    ['profile', 'email', 'https://www.googleapis.com/auth/calendar'],
-    session: true,
-}));
+router.get(
+  '/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email', 'https://www.googleapis.com/auth/calendar'],
+    session: true
+  })
+);
 
 router.get('/currentuser', isUser, (req, res) => {
-  UserController.findById({_id: req.user._id}, (err, user) =>{
+  UserController.findById({ _id: req.user._id }, (err, user) => {
     if (err) {
       return res.status(500).json(err);
     } else if (!user) {
-      return  res.status(204).json({user: null});
+      return res.status(204).json({ user: null });
     } else {
       return res.status(200).json(user);
     }
@@ -34,7 +40,7 @@ router.get('/authenticate', isAuthenticated, (req, res) => {
   res.status(200).json({ authenticated: !!req.user });
 });
 
-router.get('/filestack/credential', (req, res, next) =>{
+router.get('/filestack/credential', (req, res, next) => {
   const apikey = process.env.FILESTACK_KEY;
   const signature = process.env.FILESTACK_SIG;
   if (apikey && signature) {
@@ -52,8 +58,7 @@ router.get('/filestack/credential', (req, res, next) =>{
 
     return res.status(200).json(response);
   } else {
-    return next({message: 'Fail ', err: 'access denided'});
+    return next({ message: 'Fail ', err: 'access denided' });
   }
-
 });
 module.exports = router;
